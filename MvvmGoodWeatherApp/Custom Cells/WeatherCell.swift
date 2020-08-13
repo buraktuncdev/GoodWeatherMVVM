8b1b2577b0630e30d74881ed58305da7240085e5
Burak Tunç <buraktuncdev@gmail.com>
2020-08-13 22:58:11 +0300

    Table View and Cell are Configured

---
+++ MvvmGoodWeatherApp/Custom Cells/WeatherCell.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherCell.swift
+//  MvvmGoodWeatherApp
+//
+//  Created by Burak Tunc on 13.08.2020.
+//  Copyright © 2020 Burak Tunç. All rights reserved.
+//
+
+import UIKit
+
+
+class WeatherCell: UITableViewCell {
+    
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var temparatureLabel: UILabel!
+    
+    
+    
+}
