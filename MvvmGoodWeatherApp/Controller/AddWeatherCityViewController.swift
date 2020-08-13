e46f704621209cf92453e44c76fa716765c0a453
Burak Tunç <buraktuncdev@gmail.com>
2020-08-13 22:47:33 +0300

    first commit

---
+++ MvvmGoodWeatherApp/Controller/AddWeatherCityViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AddWeatherCityViewController.swift
+//  MvvmGoodWeatherApp
+//
+//  Created by Burak Tunc on 13.08.2020.
+//  Copyright © 2020 Burak Tunç. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddWeatherCityViewController: UIViewController {
+    
+}
---
+++ MvvmGoodWeatherApp/Controller/WeatherListTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherListTableViewController.swift
+//  MvvmGoodWeatherApp
+//
+//  Created by Burak Tunc on 13.08.2020.
+//  Copyright © 2020 Burak Tunç. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WeatherListTableViewController: UITableViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+}
