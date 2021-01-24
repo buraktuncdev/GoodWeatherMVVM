7e998941cabe31dfdae4bfb71f09e256fb9819ef
Burak Tunc <burak.tunc@check24.de>
2021-01-24 20:32:13 +0100

    Settings View Model is OK

---
+++ MvvmGoodWeatherApp/Extension/Double+Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Double+Extensions.swift
+//  MvvmGoodWeatherApp
+//
+//  Created by Burak Tunc on 24.01.21.
+//  Copyright © 2021 Burak Tunç. All rights reserved.
+//
+
+import Foundation
---
+++ MvvmGoodWeatherApp/ViewModels/SettingsViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  SettingsViewModel.swift
+//  MvvmGoodWeatherApp
+//
+//  Created by Burak Tunc on 24.01.21.
+//  Copyright © 2021 Burak Tunç. All rights reserved.
+//
+
+import Foundation
